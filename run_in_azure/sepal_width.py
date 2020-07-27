cb4d565f0840c255220b61b54f646808f6ece80e
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-07-27 13:51:53 +0100

    Add scripts to be run in azure

---
+++ run_in_azure/sepal_width.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+import pandas as pd
+
+from azure_batch_testing import average_iris_column
+
+pd.DataFrame = average_iris_column("sepal.width").to_csv(Path(__file__).parent.parent / "output" / "sepal_width.csv")
