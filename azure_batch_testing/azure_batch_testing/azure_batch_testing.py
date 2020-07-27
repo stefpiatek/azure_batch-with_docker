ed4a498316ce0a178aa52ed0efbc85c40bac8254
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-07-27 13:44:28 +0100

    Add dummy function for testing

--- azure_batch_testing/azure_batch_testing/azure_batch_testing.py
+++ azure_batch_testing/azure_batch_testing/azure_batch_testing.py
@@ -1,1 +1,12 @@
 """Main module."""
+from pathlib import Path
+
+import pandas as pd
+
+
+def average_iris_column(column):
+    return pd.read_csv(Path(__file__).parent.parent / "resources" /"iris.csv").groupby("variety").sum().loc[:, [column]]
+
+
+if __name__ == "__main__":
+    print(average_iris_column("petal.width"))
