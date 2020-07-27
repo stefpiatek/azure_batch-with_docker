d033269f640e6f68e0e193ebe9b5b83e199aef95
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-07-27 14:28:50 +0100

    Setup dockerfile

--- azure_batch_testing/azure_batch_testing/__init__.py
+++ azure_batch_testing/azure_batch_testing/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for Azure Batch Testing."""
+from .azure_batch_testing import average_iris_column
 
 __author__ = """Stef Piatek"""
 __email__ = 's.piatek@ucl.ac.uk'
 __version__ = '0.1.0'
+# expose function for use by scripts
+__all__ = ["average_iris_column"]
+
