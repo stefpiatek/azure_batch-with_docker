fbd0c608d202bfd072e9256ff3383514ceb47d4d
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-07-28 11:51:51 +0100

    Add azure batch example for editing to user docker

--- azure_batch/resources/petal_width.py
+++ azure_batch/resources/petal_width.py
@@ -5,3 +5,4 @@ import pandas as pd
 from azure_batch_testing import average_iris_column
 
 pd.DataFrame = average_iris_column("petal.width").to_csv(Path(__file__).parent.parent / "output" / "petal_width.csv")
+print("Petal width done")
--- azure_batch/scripts/sepal_width.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-import pandas as pd
-
-from azure_batch_testing import average_iris_column
-
-pd.DataFrame = average_iris_column("sepal.width").to_csv(Path(__file__).parent.parent / "output" / "sepal_width.csv")
